e0e835d82ba293acd8283721e60b7261bf88e8d5
SepriyanGunawan <sepriyandevil123@gmail.com>
2025-07-25 05:18:56 +0700

    feat:add email and password input validation using JavaScript DOM

---
+++ asset/js/sign-up.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('form');
+const inputpwd = form.querySelector('#pwd');
+const eye_open = form.querySelector('.open');
+const eye_close = form.querySelector('.close');
+const warpwd = document.querySelector('.text-warning-pass');
+const waremail = document.querySelector('.text-warning-email');
+eye_close.addEventListener('click', () => {
+  eye_close.classList.add('eye');
+  eye_open.classList.remove('eye');
+  inputpwd.type = 'text';
+});
+eye_open.addEventListener('click', () => {
+  eye_open.classList.add('eye');
+  eye_close.classList.remove('eye');
+  inputpwd.type = 'password';
+});
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const email = document.querySelector('#email');
+  const emailvalue = email.value;
+  const password = document.querySelector('#pwd').value;
+  // console.log(password);
+  let valid = true;
+  const reemail = /^[^\s@]+@gmail+\.com+$/;
+  const rg_az = /^(?=.*[a-z]).*$/;
+  const rg_AZ = /^(?=.*[A-Z]).*$/;
+  const rg_sim = /^(?=.*[!@#$%^*/><]).*$/;
+  //  email validasi
+  // const text1 = document.createElement('p');
+  if (emailvalue === '') {
+    console.log('masuk');
+    waremail.textContent = 'email tidak boleh kosong';
+  } else {
+    if (!reemail.test(emailvalue)) {
+      waremail.textContent = 'gmail tidak valid';
+    }
+  }
+
+  if (password === '') {
+    warpwd.textContent = 'password tidak boleh kosong';
+    return;
+  }
+  if (!rg_AZ.test(password)) {
+    warpwd.textContent = 'harus ada huruf besar';
+    return;
+  }
+  if (!rg_az.test(password)) {
+    warpwd.textContent = 'harus ada huruf kecil';
+    return;
+  }
+  if (!rg_sim.test(password)) {
+    warpwd.textContent = 'harus ada karakter';
+    return;
+  }
+  if (password.length < 8) {
+    warpwd.textContent = 'password harus lebih dari 8 karakter';
+    return;
+    // return;
+  }
+  alert(`email & password berhasil di buat`);
+  localStorage.setItem('name', 'Dikih Arif Wibowo');
+
+  let checkstorege = localStorage.getItem('name');
+  console.log(checkstorege);
+  if (checkstorege) {
+    window.location.href = '../index.html';
+  }
+});
