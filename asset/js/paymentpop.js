743f14f4b12de4c019c2083ed49d5431d9b9cd56
SepriyanGunawan <sepriyandevil123@gmail.com>
2025-07-27 16:09:03 +0700

    feat:Adding an interactive feature to open and close the payment modal, if the user clicks on paylatter, the modal will close.

---
+++ asset/js/paymentpop.js
@@ -0,0 +1,7 @@
+function openPop() {
+  const bg_modal = document.querySelector('.bg-modal');
+  bg_modal.style.display = bg_modal.style.display === 'none' ? 'flex' : 'none';
+}
+// function close() {
+//   //   const closePop = document.querySelector('.btn-payleter');
+// }
